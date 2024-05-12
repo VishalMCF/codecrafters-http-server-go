9bc807179dc5bb2755b945b1bfa84d14a5d6f01b
Vishal M Yadav <vishal.yadav@embibe.com>
2024-05-13 01:33:44 +0530

    added the feature to stream data of a file

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
@@ -26,9 +27,7 @@ type RequestParams struct {
 }
 
 func main() {
-
 	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -77,6 +76,24 @@ func handleGetRequest(reqParams RequestParams, conn net.Conn) error {
 			reqHeader := reqParams.headers["User-Agent"]
 			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
 				len(reqHeader), reqHeader)))
+			return nil
+		case "files":
+			directoryName := os.Args[0]
+			fileName := reqPathAndValue[2]
+			filePath, err := os.Open(directoryName + "/" + fileName)
+			if err != nil {
+				fmt.Println("Error happened while opening the file from the directory -> ", fileName)
+				return err
+			}
+			fileContent := bufio.NewReader(filePath)
+			fileData := make([]byte, 65507)
+			contentLength, err := fileContent.Read(fileData)
+			if err != nil {
+				fmt.Println("Error happened while loading the contents of the file into the bytes")
+				return err
+			}
+			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s",
+				contentLength, fileData)))
 			return nil
 		default:
 			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
